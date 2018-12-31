e7b66c5bf598091c23c2e6e65ac684105c54a47d
Nguyen Hieu Trung <trungnguyen.syncproject@gmail.com>
2018-12-31 22:24:47 +0700

    FirstCommit
    Trung

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using KnowledgeStore.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(4000%1000);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KnowledgeStore/Areas/AdminArea/Controllers/HoaHongHienHanhController.cs
@@ -0,0 +1,21 @@
+﻿using Model.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Areas.AdminArea.Controllers
+{
+    public class HoaHongHienHanhController : Controller
+    {
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
+        // GET: AdminArea/HoaHongHienHanh
+        public ActionResult Index()
+        {
+            var lshh = db.LichSuHoaHongs.ToList();
+
+            return View(lshh);
+        }
+    }
+}
